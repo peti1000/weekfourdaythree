19e1e4ef1660fe4be6b8b8d20e27acd9520b8f24
peti1000 <petije@gmail.com>
2021-11-03 14:40:35 +0100

    add method zeroToHundred

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int i = 0;
+        zeroToHundred(i);
+
+    }
+    private static void zeroToHundred(int i){
+
+        while(i <= 100){
+            System.out.println(i);
+            i++;
+        }
     }
 }
